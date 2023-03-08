3e4e3774c0a6f0bc73b2970550370ab61e0e687e
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-03-08 20:56:42 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  const dateOfBirth = "1st September, 1996"
+
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <h1 className="Header">Book Review</h1>  
+    );    
+}
+
+export default Header;
